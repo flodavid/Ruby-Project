0e95a6c75cdd8c536b5d99369207495529d9425a
flodavid <fl.david.53@gmail.com>
2016-02-22 14:54:03 +0100

    chapitre 4 + complétion readme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Simple App du Tutoriel Ruby on Rails"
+    if @titre.nil?
+      base_title
+    else
+      "#{base_title} | #{@titre}"
+    end
+  end
+
 end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,32 +3,54 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
+# Old way 
+#  before(:each) do
+#	@base_title = "Simple App du Tutoriel Ruby on Rails"
+#  end
+
+# New one (@base_title + "<TXT>" has to be replace by " #{@base_title} <TXT"
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET #home" do
     it "returns http success" do
 #	test "GET #home" do 
       get :home
-      expect(response).to have_http_status(:success)
-#	  assert_response :success
-	  assert_select "title", "Simple App du Tutoriel Ruby on Rails | Accueil"
+	  assert_response :success
+#      expect(response).to have_http_status(:success)
+	end
+
+    it "have good title" do
+    	get :home
+    	assert_select "title", :content => "#{@base_title} | Accueil"
+#		response.should have_selector("title",
+#                    :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
   describe "GET #contact" do
     it "returns http success" do
-#	test "GET #contact" do
       get :contact
-      expect(response).to have_http_status(:success)
-      assert_select "title", "Simple App du Tutoriel Ruby on Rails | Contact"
+	  assert_response :success
+	end
+
+    it "have good title" do
+	  get :contact
+      assert_select "title", :content => "#{@base_title} | Contact"
     end
   end
 
 
   describe "GET #about" do
     it "returns http success" do
-#	test "GET #about" do
       get :about
-      expect(response).to have_http_status(:success)
-      assert_select "title", "Simple App du Tutoriel Ruby on Rails | À Propos"
+	  assert_response :success
+	end
+
+    it "have good title" do
+    	get :about
+	    assert_select "title", :content => "#{@base_title} | À Propos"
     end
   end
 
